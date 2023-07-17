e0517ec603980ed169208f0963d69df665346e23
oktadha01 <oktadha01@gmail.com>
2023-07-17 14:43:51 +0700

    sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -5,7 +5,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href="" target="_blank">
                 <img src="<?= base_url(); ?>assets_adm/img/logo/logo2.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold">Site Plan</span>
+                <span class="ms-1 font-weight-bold">PT KANPA</span>
             </a>
         </div>
         <hr class="horizontal dark mt-0">
