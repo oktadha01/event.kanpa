e1fecf9fb85c7b6505e4999f0d3d16f9f160d60b
oktadha01 <oktadha01@gmail.com>
2023-07-17 11:27:59 +0700

    dash

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Customer extends CI_Controller
 {
-    public $M_Customer;
+    public $M_customer;
     public $db;
     public $uri;
     var $template = 'templates/index';
@@ -11,7 +11,7 @@ class Customer extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('M_Customer');
+        $this->load->model('M_customer');
     }
     function event()
     {
@@ -20,7 +20,7 @@ class Customer extends CI_Controller
 
         $data['bread']          = 'Customer Event ' . $nm_perum;
         $data['tittle']          = 'Customer Event ' . $nm_perum;
-        $data['data_customer'] = $this->M_Customer->m_data_customer($nm_perum);
+        $data['data_customer'] = $this->M_customer->m_data_customer($nm_perum);
         // $data['script']        = 'page/customer/setting_event_js';
         $data['content']        = 'page/customer/customer';
         $this->load->view($this->template, $data);
